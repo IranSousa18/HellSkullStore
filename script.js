101f1b460d015589cf1da127dedc5aca2c8237ac
iran <iran.csousa21@gmail.com>
2026-05-18 11:52:56 -0300

    Barra de pesquisa funcional, 10 cards feitos.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const pesquisaInput = document.getElementById("pesquisaJogos")
-const jogos = document.querySelectorAll(".card")
-pesquisaInput.addEventListener("input",  () => {
+const pesquisaInput = document.getElementById("pesquisaInput");
+const jogos = document.querySelectorAll(".card");
 
-    const valorPesquisa = pesquisaInput.value.toLowerCase()
-    jogos.forEach((jogo) => {
-        const titulo =
-        jogo.querySelector("h3")
-        .innerText
-        .toLowerCase()
-        if(titulo.includes(valorPesquisa)){
-            jogo.style.display = "block"
-        }else{
-            jogo.style.display = "none"
-        }
-    })
-})
+pesquisaInput.addEventListener("input", () => {
+  const valorPesquisa = pesquisaInput.value.toLowerCase();
+
+  jogos.forEach((jogo) => {
+    const titulo = jogo.querySelector("h3")?.innerText.toLowerCase();
+
+    if (titulo && titulo.includes(valorPesquisa)) {
+      jogo.style.display = "block";
+    } else {
+      jogo.style.display = "none";
+    }
+  });
+});
